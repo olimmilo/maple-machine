37f193187c08a879442ff0a1b30401111286cd0a
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-07-12 19:30:15 +0000

    Update maple.cpp

--- maple.cpp
+++ maple.cpp
@@ -1,4 +1,28 @@
+float feedRate = 0.0;
+float speedRate = 0.0;
+float cycleLength = 30.0;
+float cycleSpeed = 10.0;
 
+bool isFilling = FALSE; // whether the sugar spout is feeding sugar
+bool isLoading = FALSE; // whether the spindle is in loading position
+bool isReferenced = FALSE; // whether the machine is referenced
+bool isRunning = FALSE; //whether prgm is running
+
+int feedPin = 0;
+int speedPin = 0;
+int cyclenPin = 0;
+int cycspdPin = 0;
+int fillingPin = 0;
+int loadingPin = 0;
+int referencePin = 0;
+int runningPin = 0;
+
+void reference() //references the axes
+{
+		
+}
+
+void calculate
 
 void setup()
 {
