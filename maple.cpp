61cce422cc5d82251d8a5964993720d86890d7ee
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-07-14 17:53:45 -0400

    Update maple.cpp

--- maple.cpp
+++ maple.cpp
@@ -1,31 +1,23 @@
 int ERROR = 0;
 
-float spindleX = 60.0;
-float maxA = 1000.0;
-float spindleOffset = 90.0;
-float spoutOffset = 60.0;
+float bedLen = 0.0;
+float maxSpeed = 100.0;
 
 float stopX = 60.0;
 
 float feedRate = 0.0;
 float speedRate = 0.0;
-float cycleLength = 30.0;
 float cycleOffset = 10.0;
 
-bool isFeeding = FALSE; // whether the sugar spout is feeding sugar
-bool isLoading = FALSE; // whether the spindle is in loading position
 bool isReferenced = FALSE; // whether the machine is referenced
 bool isRunning = FALSE; //whether prgm is running
 
-int feedPin = 0;
-int speedPin = 0;
-int cyclenPin = 0;
-int cycoffPin = 0;
-int fillingPin = 0;
-int loadingPin = 0;
-int referencePin = 0;
-int runningPin = 0;
-int stopPin = 0;
+int FeedPotPin = 0;
+int SpeedPotPin = 0;
+int CycO ffUpBtnPin = 0;
+int CycOffDownBtnPin = 0;
+int RefBtnPin = 0;
+int RunBtnPin = 0;
 
 void reference() //references the axes
 {
@@ -45,21 +37,6 @@ void calculate() //calculate the switch points on the x axis
 	{
 		ERROR = 1;
 	}
-
-}
-
-void load()
-{
-
-}
-
-void unload()
-{
-
-}
-
-void feed()
-{
 
 }
 
@@ -88,18 +65,6 @@ void loop()
 		if(ERROR ==0 && isReferenced == FALSE)
 		{
 			reference();
-		}
-		if(ERROR ==0 && isLoading == TRUE)
-		{
-			load();
-		}
-		if(ERROR ==0 && isLoading == FALSE)
-		{
-			unload();
-		}
-		if(ERROR ==0 && isFeeding == TRUE)
-		{
-			feed();
 		}
 		if(ERROR ==0 && isReferenced == TRUE && isLoading == FALSE && isRunning == TRUE)
 		{
